db9cf8455b3757dffff18e90bfdd4f15da951ab0
kevin <kevin@kevin.kevin>
2016-03-02 20:38:20 +0100

    Accelerometer + Gyroscope + Magnetometer + Step Counter + Thermometer

---
+++ android/src/main/java/com/sensormanager/MagnetometerRecord.java
@@ -0,0 +1,78 @@
+package com.sensormanager;
+
+import android.os.Bundle;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.util.Log;
+import android.support.annotation.Nullable;
+
+import java.io.*;
+import java.util.Date;
+import java.util.Timer;
+
+import com.facebook.react.bridge.Arguments;
+import com.facebook.react.bridge.ReactContext;
+import com.facebook.react.bridge.WritableMap;
+import com.facebook.react.modules.core.DeviceEventManagerModule;
+import com.facebook.react.bridge.ReactApplicationContext;
+
+public class MagnetometerRecord implements SensorEventListener {
+
+    private SensorManager mSensorManager;
+    private Sensor mMagnetometer;
+    private long lastUpdate = 0;
+    private int i = 0, n = 0;
+	private int delay;
+
+	private ReactContext mReactContext;
+	private Arguments mArguments;
+
+
+    public MagnetometerRecord(ReactApplicationContext reactContext, int delay) {
+		this.delay = delay;
+        mSensorManager = (SensorManager)reactContext.getSystemService(reactContext.SENSOR_SERVICE);
+        mMagnetometer = mSensorManager.getDefaultSensor(Sensor.TYPE_MAGNETIC_FIELD);
+        mSensorManager.registerListener(this, mMagnetometer, SensorManager.SENSOR_DELAY_FASTEST);
+		mReactContext = reactContext;
+    }
+
+    public void stop() {
+        mSensorManager.unregisterListener(this);
+    }
+
+	private void sendEvent(String eventName, @Nullable WritableMap params)
+	{
+		try {
+			mReactContext 
+				.getJSModule(DeviceEventManagerModule.RCTDeviceEventEmitter.class) 
+				.emit(eventName, params);
+		} catch (RuntimeException e) {
+			Log.e("ERROR", "java.lang.RuntimeException: Trying to invoke JS before CatalystInstance has been set!");
+		}
+	}
+
+    @Override
+    public void onSensorChanged(SensorEvent sensorEvent) {
+        Sensor mySensor = sensorEvent.sensor;
+		WritableMap map = mArguments.createMap();
+
+        if (mySensor.getType() == Sensor.TYPE_MAGNETIC_FIELD) {
+            long curTime = System.currentTimeMillis();
+            i++;
+            if ((curTime - lastUpdate) > 92) {
+                i = 0;
+				map.putDouble("x", sensorEvent.values[0]);
+				map.putDouble("y", sensorEvent.values[1]);
+				map.putDouble("z", sensorEvent.values[2]);
+				sendEvent("magValue", map);
+                lastUpdate = curTime;
+            }
+        }
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+    }
+}
---
+++ android/src/main/java/com/sensormanager/SensorManagerModule.java
@@ -0,0 +1,103 @@
+package com.sensormanager;
+
+import com.facebook.react.bridge.NativeModule;
+import com.facebook.react.bridge.ReactApplicationContext;
+import com.facebook.react.bridge.ReactContext;
+import com.facebook.react.bridge.ReactContextBaseJavaModule;
+import com.facebook.react.bridge.ReactMethod;
+import com.facebook.react.bridge.Callback;
+
+import android.util.Log;
+
+public class SensorManagerModule extends ReactContextBaseJavaModule {
+    private static final String		REACT_CLASS = "SensorManager";
+    private AccelerometerRecord		mAccelerometerRecord;
+	private GyroscopeRecord 		mGyroscopeRecord;
+	private MagnetometerRecord		mMagnetometerRecord;
+	private StepCounterRecord		mStepCounterRecord;
+	private ThermometerRecord		mThermometer;
+
+	private ReactApplicationContext	mReactContext;
+
+    @Override
+    public String getName() {
+        return REACT_CLASS;
+    }
+
+    public SensorManagerModule(ReactApplicationContext reactContext) {
+		super(reactContext);
+		mReactContext = reactContext;
+    }
+
+    @ReactMethod
+    public void startAccelerometer(int delay) {
+    	mAccelerometerRecord = new AccelerometerRecord(mReactContext, delay);
+    }
+
+    @ReactMethod
+    public void stopAccelerometer() {
+		mAccelerometerRecord.stop();
+    }
+
+    @ReactMethod
+    public void startGyroscope(int delay) {
+    	mGyroscopeRecord = new GyroscopeRecord(mReactContext, delay);
+    }
+
+    @ReactMethod
+    public void stopGyroscope() {
+		mAccelerometerRecord.stop();
+    }
+
+    @ReactMethod
+    public void startMagnetometer(int delay) {
+    	mMagnetometerRecord = new MagnetometerRecord(mReactContext, delay);
+    }
+
+    @ReactMethod
+    public void stopMagnetometer() {
+		mAccelerometerRecord.stop();
+    }
+
+    @ReactMethod
+    public void startStepCounter(int delay) {
+    	mStepCounterRecord = new StepCounterRecord(mReactContext, delay);
+    }
+
+    @ReactMethod
+    public void stopStepCounter() {
+		mStepCounterRecord.stop();
+    }
+
+    @ReactMethod
+    public void startThermometer(int delay) {
+    	mThermometer = new ThermometerRecord(mReactContext, delay);
+    }
+
+    @ReactMethod
+    public void stopThermometer() {
+		mThermometer.stop();
+    }
+
+	/*
+    @Override
+    public ReactBarcodeScannerView createViewInstance(ThemedReactContext context) {
+    }
+
+    @Override
+    public void onDropViewInstance(ReactBarcodeScannerView view) {
+    }
+
+    @Override
+    public void onHostResume() {
+    }
+
+    @Override
+    public void onHostPause() {
+    }
+
+    @Override
+    public void onHostDestroy() {
+    }
+    */
+}
